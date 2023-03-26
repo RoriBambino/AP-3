de16e4139ba02208df6242b02a595a148731bcf1
RoriBambino <122678009+RoriBambino@users.noreply.github.com>
2023-03-27 07:47:25 +0800

    eight commit

---
+++ Module 6/Module6/Module6/Module7Performance.aspx
@@ -0,0 +1,92 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Module7Performance.aspx.cs" Inherits="Module6.WebForm3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 806px;
+        }
+        .auto-style2 {
+            position: absolute;
+            top: 20px;
+            left: 463px;
+            z-index: 1;
+        }
+        .auto-style3 {
+            position: absolute;
+            top: 67px;
+            left: 265px;
+            z-index: 1;
+        }
+        .auto-style4 {
+            position: absolute;
+            top: 149px;
+            left: 476px;
+            z-index: 1;
+        }
+        .auto-style5 {
+            position: absolute;
+            top: 64px;
+            left: 355px;
+            z-index: 1;
+            width: 340px;
+            }
+        .auto-style6 {
+            position: absolute;
+            top: 101px;
+            left: 261px;
+            z-index: 1;
+        }
+        .auto-style7 {
+            position: absolute;
+            top: 98px;
+            left: 359px;
+            z-index: 1;
+            width: 338px;
+        }
+        .auto-style8 {
+            position: absolute;
+            top: 326px;
+            left: 462px;
+            z-index: 1;
+        }
+        .auto-style9 {
+            position: absolute;
+            top: 209px;
+            left: 396px;
+            z-index: 1;
+        }
+        .auto-style10 {
+            position: absolute;
+            top: 243px;
+            left: 461px;
+            z-index: 1;
+        }
+        .auto-style11 {
+            position: absolute;
+            top: 280px;
+            left: 462px;
+            z-index: 1;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <asp:Label ID="Label1" runat="server" CssClass="auto-style2" Text="F.L.A.M.E.S"></asp:Label>
+            <asp:Label ID="Label2" runat="server" CssClass="auto-style3" Text="First Name:"></asp:Label>
+            <asp:Button ID="Result" runat="server" CssClass="auto-style4" Text="Result" OnClick="Result_Click" />
+            <asp:TextBox ID="f_username" runat="server" CssClass="auto-style5"></asp:TextBox>
+            <asp:Label ID="Label3" runat="server" CssClass="auto-style6" Text="Second Name: "></asp:Label>
+            <asp:TextBox ID="s_username" runat="server" CssClass="auto-style7"></asp:TextBox>
+            <asp:Label ID="result_flames" runat="server" CssClass="auto-style8"></asp:Label>
+            <asp:Label ID="Label4" runat="server" CssClass="auto-style9" Text="The Result of using F.L.A.M.E.S of:"></asp:Label>
+            <asp:Label ID="result_name1" runat="server" CssClass="auto-style10"></asp:Label>
+            <asp:Label ID="result_name2" runat="server" CssClass="auto-style11"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Module 6/Module6/Module6/Module7Performance.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Module6
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Result_Click(object sender, EventArgs e)
+        {
+            // Variable 
+            String name1 = f_username.Text.ToUpper();
+            String name2 = s_username.Text.ToUpper();
+            int l = 1, string_count = 0, recursive_count = 0, fc = 5;
+            // Make the String to Array
+            char[] name1_char = name1.ToCharArray();
+            char[] name2_char = name2.ToCharArray();
+            String flames_string = "flames";
+            char[] flames_char = flames_string.ToCharArray();
+            int name1_len = name1.Length;
+            int name2_len = name2.Length;
+            int total_len = name1_len + name2_len;
+          
+          
+            result_name1.Text = name1;
+            result_name2.Text = name2;
+            // Checks if name1 and name2 have similar letters
+            for (int i = 0; i < name1_len; i++)
+            {
+                char a = name1_char[i];
+                for (int j = 0; j < name2_len; j++)
+                {
+                    
+                    if (a == name2_char[j])
+                    {                  
+                        name1_char[i] = name2_char[j] = '-';
+                        string_count += 2;
+                        break;
+                    } 
+                }
+            }
+            recursive_count = total_len - string_count;
+            // Count the number of different letters then checks if the remaining letter of the word Flames
+            int k = 0; 
+            while(k >= 0)
+            {
+                if(l == recursive_count)
+                {
+                    for (int a = k; a < flames_string.Length - 1; a++)
+                    {
+                        flames_char[a] = flames_char[a + 1]; 
+                    }
+                    flames_char[flames_char.Length - 1] = '0';
+                    fc--;
+                    k--;
+                    l = 0;
+                }
+                if (k == fc)
+                {
+                    k = -1;
+
+                }
+                if (fc == 0)
+                {
+                    break;
+                }
+                l++;
+                k++;
+            }
+            char result = flames_char[0];
+            switch (result)
+            {
+                case 'f':
+                    result_flames.Text = "Friends";
+                    break;
+                case 'l':
+                    result_flames.Text = "Lovers";
+                    break;
+                case 'a':
+                    result_flames.Text = "Admirers";
+                    break;
+                case 'm':
+                    result_flames.Text = "Marriage";
+                    break;
+                case 'e':
+                    result_flames.Text = "Enemies";
+                    break;
+                case 's':
+                    result_flames.Text = "Secret Lovers";
+                    break;
+
+            }
+            
+        }
+    }
+}
